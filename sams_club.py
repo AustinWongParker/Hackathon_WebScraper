d615c136d735543295839586d3d6aac840385413
Jessica <peters29@stolaf.edu>
2020-04-09 18:30:57 -0500

    testing

---
+++ sams_club.py
@@ -0,0 +1,26 @@
+#the file for scraping sams club websites
+#website link for TP: https://www.samsclub.com/s/toilet%20paper
+
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get("https://www.samsclub.com/s/toilet%20paper/")
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+
+# urls = []
+# for h2_tag in soup.find_all('h2'):
+#     a_tag = h2_tag.find('a')
+#     urls.append(a_tag.attrs['href'])
+
+# price = soup.find_all('div', {'class': 'sc-channel-price'})[0].find_all('span', {'class': 'Price-group'}).title
+price = soup.find_all('span', {'class': 'Price-group'})[0].title
+print(price)
+    # print(price) # Just to see it on the screen / cmd for testing purposes
+    # return price
+
+urls = []
+for spans in soup.find_all('span', {'class': 'Price-group'})[0].title:
+    urls.append(spans)
+
+print(urls)
