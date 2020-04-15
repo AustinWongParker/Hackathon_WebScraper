229d0be9ebf6024c460756e4770a4052d00ea5e0
Austin Wong <wongparker97@gmail.com>
2020-04-14 20:27:49 -0500

    website is alive barely

--- flask_website/__init__.py
+++ flask_website/__init__.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+# Front-end for hackathon
+# flask python
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask, render_template, redirect, url_for
+import sys
+app = Flask(__name__) # app is an instance of the flask class.
+
+@app.route('/') # route decorator tells flask which URL to go through.
+def home():
+    return render_template('layout.html')
+
+if __name__ == "__main__":
+    #app.run()
+    app.run(debug=True) # uncomment for debugging
