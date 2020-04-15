b199ac5ea0e8a8633df3db35745f914c640fd777
ninjanadya <posto018@umn.edu>
2020-04-14 20:01:47 -0500

    Updated heb, added color pallete hex and rgb values

---
+++ heb_scraper.py
@@ -0,0 +1,30 @@
+ 
+# OK SO APPARENTLY KROGER DOESN'T LIKE WEBSCRAPING
+# Scraping for H-E-B bc Texas
+# Nadya Postolaki
+
+import requests
+from bs4 import BeautifulSoup
+
+print('Toilet Paper:')
+
+result = requests.get("https://www.heb.com/search/?q=toilet+paper")
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+
+prices = []
+
+for divvs in soup.find_all('div', {'class': 'cat-price'}):
+	spans = divvs.find('span', {'class': 'cat-price-number'})
+	prices.append(spans.text)
+
+
+#TO-DO: get rid of random characters in prices array.
+for str in prices:
+	newStr = str.replace('\n','').replace('each','').replace(' ','')
+	print(newStr)
+
+
+
+
+print(prices)
